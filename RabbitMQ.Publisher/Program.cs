b882ea9fbc17e9e3a1158fbf7def857a692bab10
BasarErsoy <basarersoy@gmail.com>
2023-06-16 10:46:30 +0300

    Add project files.

---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,20 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("\tamqps://qdnmlash:1GBY31rv-Kwtr51ZZVjxeerZGp6X6ZlQ@puffin.rmq2.cloudamqp.com/qdnmlash");
+using var connnection = factory.CreateConnection();
+
+var channel = connnection.CreateModel();
+
+channel.QueueDeclare("hello-queue",true,false,false);
+
+string message = "hello world";
+
+var messageBody = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(string.Empty,"hello-queue",null,messageBody);
+
+Console.WriteLine("Mesaj Gönderildi");
+
+Console.ReadLine(); 
---
+++ RabbitMQ.Subscriber/Program.cs
@@ -0,0 +1,26 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("\tamqps://qdnmlash:1GBY31rv-Kwtr51ZZVjxeerZGp6X6ZlQ@puffin.rmq2.cloudamqp.com/qdnmlash");
+using var connnection = factory.CreateConnection();
+
+var channel = connnection.CreateModel();
+
+//channel.QueueDeclare("hello-queue", true, false, false);
+
+var subscriber = new EventingBasicConsumer(channel);
+
+channel.BasicConsume("hello-queue", true, subscriber);
+
+subscriber.Received += Subscriber_Received;
+
+void Subscriber_Received(object? sender, BasicDeliverEventArgs e)
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+    Console.WriteLine(message);
+}
+
+Console.ReadLine();
